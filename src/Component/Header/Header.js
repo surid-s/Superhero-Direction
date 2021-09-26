43c6eda659e6768c2989456aa92d5f503bc9773d
surid-s <maruf15-9549@diu.edu.bd>
2021-09-26 15:33:27 +0600

    fontawesome added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,11 +3,11 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className="App">
             <header className="header-container">
                 <h1 className="heading">Select your favourite tv series</h1>
                 <h4>A group of programs created or adapted for television broadcast with a common series title.</h4>
-                <h2>Total: 150 Million</h2>
+                <h2>Total: $150 Million</h2>
             </header>
         </div>
     );
--- src/Component/Series/Series.js
+++ src/Component/Series/Series.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Series.css';
 
 const Series = (props) => {
     // console.log(props)
     const { name, budget, creator, genre, img, rating } = props.series;
+    const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="single-series-container">
             <img src={img} alt="" />
@@ -12,7 +15,7 @@ const Series = (props) => {
             <h4>Genre: {genre}</h4>
             <h4>Rating: {rating}</h4>
             <h4>Budget: {budget}</h4>
-            <button onClick={() => props.addToSummery(props.series)} className="fav-btn">Add to Favourite</button>
+            <button onClick={() => props.addToSummery(props.series)} className="fav-btn">{element} <span className="fav-btn-txt">Add to Favourite</span> </button>
         </div>
     );
 };
